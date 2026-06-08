9eb49e7b1aeefd7bc505ceecb46cd10915219c09
andres <andrescc2817@gmail.com>
2026-06-08 09:23:16 -0400

    componente ejerciciocard

---
+++ app/componentes/ejercicio.tsx
@@ -0,0 +1,13 @@
+import "./ejercicio.css"
+import images from "../imagenes/images"
+export default function ejercicio() {
+    return (
+        <div className="ejercicio">
+            <h1>Exercicios</h1>
+            
+          
+        </div>
+
+
+    )
+}
