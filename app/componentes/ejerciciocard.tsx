1372817e8f1a254b2ac50ab00f6276973473e018
andres <andrescc2817@gmail.com>
2026-06-08 09:33:19 -0400

    componente info realizado

---
+++ app/componentes/ejerciciocard.tsx
@@ -0,0 +1,6 @@
+import "./ejerciciocard.css"
+import info from "./info"
+export default function ejerciciocard() {
+    return (
+        <div className="card">
+            <imp src="../imagenes/images.jfif"
---
+++ app/componentes/info.tsx
@@ -0,0 +1,10 @@
+import "./info.css"
+
+export default function info() {
+    return (
+        <div className="info">
+            <h2 className="nombre">Puxada fontal</h2>
+            <p className="serie">3 séries x 12 repetições</p>
+        </div>
+    )
+}
